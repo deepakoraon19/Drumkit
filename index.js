9fbd6980324436ba32f3b3a03c9a6119bdc11b1a
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-02-07 18:43:36 +0530

    click

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Amit chodu")
+var x=0;
+var times=document.querySelectorAll(".drum").length;
+var audio = new Audio("sounds/crash.mp3")
+for(x=0;x<times;x++)
+{
+    document.querySelectorAll("button")[x].addEventListener("click", function nacho(){
+        this.style.color="#fff";
+    });
+    
+}
