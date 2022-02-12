e8dc780637f96491dce48fd67ad45b8a12c47f45
unknown <deepak.19ranchi@gmail.com>
2022-02-12 15:27:44 +0530

    keyboard bind

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var size = document.querySelectorAll(".drum").length;
 var i;
 
@@ -7,15 +6,39 @@ for(i=0;i<size;i++)
     document.querySelectorAll(".drum")[i].addEventListener("click", function zinda(){
         alert("zindabad");
     })
-=======
-var x=0;
-var times=document.querySelectorAll(".drum").length;
-var audio = new Audio("sounds/crash.mp3")
-for(x=0;x<times;x++)
+}
+
+document.addEventListener("keypress", function(event)
 {
-    document.querySelectorAll("button")[x].addEventListener("click", function nacho(){
-        this.style.color="#fff";
-    });
-    
->>>>>>> 9fbd6980324436ba32f3b3a03c9a6119bdc11b1a
+    // alert(event.key);
+    makeSound(event.key);
+})
+
+
+function makeSound(key)
+{
+    switch(key)
+    {
+        case 'w':   var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                    break;
+        case 'a':   var crash = new Audio("sounds/crash.mp3");
+                    crash.play();
+                    break;
+        case 's':   var kick_bass = new Audio("sounds/kick-bass.mp3");
+                    kick_bass.play();
+                    break;            
+        case 'd':   var tom_1 = new Audio("sounds/tom-1.mp3");
+                    tom_1.play();
+                    break;
+        case 'j':   var tom_2 = new Audio("sounds/tom-2.mp3");
+                    tom_2.play();
+                    break;
+        case 'k':   var tom_3 = new Audio("sounds/tom-3.mp3");
+                    tom_3.play();
+                    break;
+        case 'l':   var tom_4 = new Audio("sounds/tom-4.mp3");
+                    tom_4.play();
+                    break;
+                }
 }
